96b2468b703f5cf00971c0756b88db24d2977d7a
benmajin <benjamin.voissiere@hotmail.fr>
2022-11-17 21:50:45 +0100

    fixed mobile hover issue sort of

--- src/Components/ui/ProjectCard.tsx
+++ src/Components/ui/ProjectCard.tsx
@@ -19,12 +19,12 @@ const ProjectCard = ({
 }: TProject) => (
 	<div className="group relative overflow-hidden">
 		<Image src={image} width={300} height={400} alt="project image" />
-		<div className="absolute bottom-0  w-full translate-y-2/3 transform overflow-hidden bg-black/50 text-mainWhite transition-all delay-500 duration-500 group-hover:-translate-y-0 group-hover:bg-black/90">
+		<div className="absolute bottom-0  w-full transform overflow-hidden bg-black/90 text-mainWhite transition-all delay-500 duration-500 md:translate-y-2/3 md:bg-black/50 md:group-hover:-translate-y-0 md:group-hover:bg-black/90">
 			<div className="max-w-fit py-6">
 				<span className="pl-4 text-2xl font-bold">{title}</span>
-				<div className="h-1 w-0 transform bg-mainRed delay-75 duration-500 group-hover:w-full" />
+				<div className="h-1 w-0 transform bg-mainRed delay-75 duration-500 md:group-hover:w-full" />
 			</div>
-			<div className="px-4 opacity-0 transition-all delay-1000 duration-300 group-hover:opacity-100">
+			<div className="px-4 transition-all delay-1000 duration-300 md:opacity-0 md:group-hover:opacity-100">
 				<p className=" ">{desc}</p>
 				<div className="flex space-x-3 py-4">
 					<ButtonProjects destination={githubLink} label="Github" />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+
 	theme: {
 		extend: {
 			colors: {
@@ -8,6 +9,9 @@ module.exports = {
 				darkBlue: "#1F2235",
 				mainRed: "#FF4A57",
 				mainWhite: "#F6F1EC",
+			},
+			screens: {
+				"hover-hover": { raw: "(hover: hover)" },
 			},
 		},
 	},
