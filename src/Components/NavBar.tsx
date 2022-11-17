b7003b380930a3b47ec27f0fc83e560b784d078a
benmajin <benjamin.voissiere@hotmail.fr>
2022-11-17 16:35:46 +0100

    handle click outside of nav

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,22 +1,29 @@
 import Image from "next/image"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import LogoSmall from "../Assets/images/logo_small.png"
 import BurgerIcon from "./ui/BurgerIcon"
 import MobileMenu from "./ui/MobileMenu"
 import NavLink from "./ui/NavLink"
 import NavLinks from "./ui/NavLinks"
-import { useWindowSize } from "usehooks-ts"
+import { useWindowSize, useOnClickOutside } from "usehooks-ts"
 
 const NavBar = () => {
 	const [isOpen, setIsOpen] = useState(false)
 	const { width } = useWindowSize()
+	const ref = useRef(null)
 
 	useEffect(() => {
 		if (width >= 640) setIsOpen(false)
 	}, [width])
 
+	const handleClickOutside = () => {
+		setIsOpen(false)
+	}
+
+	useOnClickOutside(ref, handleClickOutside)
+
 	return (
-		<nav className="relative bg-darkBlue text-mainWhite">
+		<nav className="relative bg-darkBlue text-mainWhite" ref={ref}>
 			<div className="mx-auto flex items-center justify-between px-4 py-6 lg:max-w-screen-xl">
 				<Image src={LogoSmall} alt="logo" height={125} width={125} />
 
