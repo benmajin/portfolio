006607e9da3ae77e67e4a9d1180179111c124f8b
benmajin <benjamin.voissiere@hotmail.fr>
2022-11-21 09:59:45 +0100

    added on project

--- src/Components/ui/ProjectCard.tsx
+++ src/Components/ui/ProjectCard.tsx
@@ -17,9 +17,9 @@ const ProjectCard = ({
 	projectLink,
 	githubLink,
 }: TProject) => (
-	<div className="group relative overflow-hidden">
+	<div className="group relative max-h-96 overflow-hidden">
 		<Image src={image} width={300} height={400} alt="project image" />
-		<div className="absolute bottom-0  w-full transform overflow-hidden bg-black/90 text-mainWhite transition-all delay-500 duration-500 md:translate-y-2/3 md:bg-black/50 md:group-hover:-translate-y-0 md:group-hover:bg-black/90">
+		<div className="absolute bottom-0  w-full transform overflow-hidden bg-black/90 text-mainWhite transition-all delay-500 duration-500 md:translate-y-2/3 md:bg-black/60 md:group-hover:-translate-y-0 md:group-hover:bg-black/90">
 			<div className="max-w-fit py-6">
 				<span className="pl-4 text-2xl font-bold">{title}</span>
 				<div className="h-1 w-0 transform bg-mainRed delay-75 duration-500 md:group-hover:w-full" />
@@ -27,7 +27,7 @@ const ProjectCard = ({
 			<div className="px-4 transition-all delay-1000 duration-300 md:opacity-0 md:group-hover:opacity-100">
 				<p className=" ">{desc}</p>
 				<div className="flex space-x-3 py-4">
-					<ButtonProjects destination={githubLink} label="Github" />
+					{githubLink && <ButtonProjects destination={githubLink} label="Github" />}
 					{projectLink && <ButtonProjects destination={projectLink} label="Site" />}
 				</div>
 			</div>
--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,40 +1,42 @@
-import Image4Saisons from "../Assets/images/ProjectsImages/4saison1.webp"
+import TodoImage from "../Assets/images/ProjectsImages/TodoImage.png"
+import ProductPageImage from "../Assets/images/ProjectsImages/ProductPageImage.png"
+import QuizzAppImage from "../Assets/images/ProjectsImages/QuizzApp.png"
 
 export const projects = [
 	{
 		id: 1,
-		title: "Aux 4 Saisons",
+		title: "Holly Quizz",
 		desc:
-			"Site de type e-commerce. Tous les éléments d'un site e-commerce sont représentés. Réalisé entièrement avec HTLM / CSS - PHP et JavaScript",
-		projectLink: undefined,
-		githubLink: "",
-		image: Image4Saisons.src,
+			"Site de quizz sur les célébirtés, les films, les séries TV, la musique ou tout ce qui touche à Hollywood. Next JS / Prisma / Tailwaind / PlanetScale",
+		projectLink: "https://celeb-quizz.vercel.app",
+		githubLink: undefined,
+		image: QuizzAppImage.src,
 	},
 	{
 		id: 2,
-		title: "Aux 4 Saisons",
+		title: "Todo App",
 		desc:
-			"Site de type e-commerce. Tous les éléments d'un site e-commerce sont représentés. Réalisé entièrement avec HTLM / CSS - PHP et JavaScript",
-		projectLink: undefined,
-		githubLink: "",
-		image: Image4Saisons.src,
+			'Petite todo liste application. Dark Mode. Possibilité de trier les choses à faire en fonction de leur état. Ajout et suppression de "todo". Vanilla JS',
+		projectLink: "https://brilliant-lamington-5b4864.netlify.app/",
+		githubLink: "https://github.com/benmajin/todo-app",
+		image: TodoImage.src,
 	},
 	{
 		id: 3,
-		title: "Aux 4 Saisons",
+		title: "Single Product Page",
 		desc:
-			"Site de type e-commerce. Tous les éléments d'un site e-commerce sont représentés. Réalisé entièrement avec HTLM / CSS - PHP et JavaScript",
-		projectLink: undefined,
-		githubLink: "",
-		image: Image4Saisons.src,
+			"Page d'un produit. Possibilité d'ajouter le produit au panier, caroussel d'images. Page responsive . Réalisé en React avec ViteJS",
+		projectLink: "https://meek-croissant-1a0fcb.netlify.app/",
+		githubLink: "https://github.com/benmajin/product-page",
+		image: ProductPageImage.src,
 	},
-	{
-		id: 4,
-		title: "Aux 4 Saisons",
-		desc:
-			"Site de type e-commerce. Tous les éléments d'un site e-commerce sont représentés. Réalisé entièrement avec HTLM / CSS - PHP et JavaScript",
-		projectLink: undefined,
-		githubLink: "",
-		image: Image4Saisons.src,
-	},
+	// {
+	// 	id: 4,
+	// 	title: "Aux 4 Saisons",
+	// 	desc:
+	// 		"Site de type e-commerce. Tous les éléments d'un site e-commerce sont représentés. Réalisé entièrement avec HTLM / CSS - PHP et JavaScript",
+	// 	projectLink: undefined,
+	// 	githubLink: "",
+	// 	image: Image4Saisons.src,
+	// },
 ]
